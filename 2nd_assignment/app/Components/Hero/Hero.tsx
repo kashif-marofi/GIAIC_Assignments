c9640e34c1e04c8469a000325849fa87d1d657d1
Kashif Marofi <marofikashif@gmail.com>
2024-10-16 17:43:52 +0500

    Second commit

--- 2nd_assignment/app/Components/Hero/Hero.tsx
+++ 2nd_assignment/app/Components/Hero/Hero.tsx
@@ -4,10 +4,23 @@ const Hero = () => {
   return (
     <div>
     <section>
-        <p className={style.paragraph}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        <p className={style.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
             A in nostrum enim cumque dolore. Pariatur eos ut, beatae harum,
             iure laboriosa expedita quos ducimus a repudiandae numquam 
             nostrum quo doloribus.</p>
+        <p className={style.paragraph}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Nihil consectetur, maxime mollitia vero provident ad dolorum ex earum quasi corporis, 
+             inventore fugit,accusamus quis architecto placeat voluptatum 
+             laboriosam adipisci? Nesciunt?</p>   
+        <p className={style.paagraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit blanditiis quae distinctio molestias
+            voluptatibus, quaerat adipisci animi vel voluptate laudantium,
+            perspiciatis dolor ullam cumque sit dicta iure sed voluptatem 
+            odio.
+        </p>      
       </section>
     </div>
   )
